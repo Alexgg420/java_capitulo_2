866a2906deb4fcf4d4c749c749abb24ea8f94543
Alexgg420 <alexgg888999@gmail.com>
2022-10-13 10:21:49 +0200

    Ejercicio1

---
+++ Ejercicio1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Ejercicio1{
+    public static void main(String[] args){
+        int x;
+        x = 144;
+        int y;
+        y = 999;
+        System.out.println("Hemos creado dos variables, 'x' e 'y'");
+        System.out.println("x = 144");
+        System.out.println("y = 999");
+        System.out.println("Ahora se muestran en pantalla cuatro operaciones con estas dos variables");
+        System.out.println("x + y = "+(x+y));
+        System.out.println("x - y = "+(x-y));
+        System.out.println("x * y = "+(x*y));
+        System.out.println("y / x = "+((double)y/(double)x));
+    }
+}
